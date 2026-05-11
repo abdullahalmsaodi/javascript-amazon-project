96bf8acda4de0e38a8f8f14a9de5d6d6663639fe
abdullahalmsaodi <abdullah.almsaodi@gmail.com>
2026-05-11 21:51:38 +0300

    add: make add cart quantity icon interactive

--- scrips/amazon.js
+++ scrips/amazon.js
@@ -69,6 +69,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
         quantity: 1,
       });
     }
-    console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((itme) => {
+      cartQuantity += itme.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
   });
 });
